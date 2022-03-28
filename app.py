0a3980f1386cc27d72c3ebcfe9043085f36224af
Marc Evald <marc.evald@gmail.com>
2022-03-28 14:26:25 +0200

    About to get online

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import pandas as pd
 import json
 import plotly
 import plotly.express as px
-from sqlalchemy import create_engine
-import pymysql
+from sqlalchemy import create_engine, insert
+import pymysql 
+import datetime
+from operator import itemgetter
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    sqlEngine = create_engine('mysql+pymysql://root:root@localhost/db', echo=False)
+def formatRows(rows):
+    for r, row in enumerate(rows):
+        for c, col in enumerate(row):
+            if c == 1:
+                rows[r][c] = f"{col:.2f} Kg"
+            if c == 2:
+                rows[r][c] = f"{col:.2f} Kg"
+            if c == 3:
+                rows[r][c] = f"{col:.2f} %"
+            if c == 4:
+                rows[r][c] = f"{col:.2f} Kg"
+            if c == 5:
+                rows[r][c] = f"{col:.2f} Kg/m²"
+    return rows
+
+def getUsersAndWeightList():
     frameUsers = pd.read_sql("select * from Users", con=sqlEngine);
    
-    userids = list(frameUsers.id)
     users = list(frameUsers.User)
+    heights = list(frameUsers.Height)
     
-    weightList = []
+    rows = []
 
-    for i, user in enumerate(userids):
-        frameWeights = pd.read_sql(f"select * from Weights where User = {user}", con=sqlEngine)
+    for i, user in enumerate(users):
+        frameWeights = pd.read_sql(f"select * from Weights where `User` = '{user}'", con=sqlEngine)
 
-        orderDate = pd.read_sql(f"SELECT * FROM Weights where User = {user} ORDER BY Time", con=sqlEngine)
+        orderDate = pd.read_sql(f"SELECT * FROM Weights where `User` = '{user}' ORDER BY Time", con=sqlEngine)
 
         sinceLast = 0
         weightLoss = 0 
         relative = 0
         
         if len(frameWeights) > 0:
+            
+            #Change order of date
+            orderDate['Time'] = orderDate['Time'].apply(lambda x: pd.Timestamp(x).strftime('%d-%m-%Y'))
+
+            timestamp = orderDate['Time'].iloc[-1]
             newestEntry = orderDate['Weight'].iloc[-1]
+            bmi = calculateBMI(heights[i], newestEntry)
         else:
             newestEntry = 0
+            bmi = 0
 
         if len(orderDate) > 1:
             firstEntry = orderDate['Weight'].iloc[0]
@@ -38,10 +59,79 @@ def hello_world():
             sinceLast = lastEntry - newestEntry
             weightLoss = firstEntry - newestEntry
             relative = ( weightLoss / firstEntry ) * 100
-            
+           
+        rows.append( [users[i], newestEntry, weightLoss, relative, sinceLast, bmi, timestamp])
+
+    return users, rows
+
+def calculateBMI(height, weight):
+    bmi = weight / (height**2)
+    return bmi
+
+app = Flask(__name__)
+sqlEngine = create_engine('mysql+pymysql://root:root@localhost/db', echo=False)
+
+@app.route('/')
+def hello_world():    
         
-        weightList.append( [users[i], newestEntry, weightLoss, relative, sinceLast])
+    users, rows = getUsersAndWeightList()
 
-    return render_template('index.htm', users=users, weights=weightList )
+    rows = formatRows(rows) 
 
+    return render_template('index.htm', users=users, rows=rows)
 
+@app.route('/enterweight', methods = ['POST'])
+def enterWeight():
+    user = request.form['user']
+    weight = request.form['weight']
+    
+    time = datetime.datetime.now()
+    time = f"{time.year}-{time.month}-{time.day} {time.hour}:{time.minute}:{time.second}"
+
+    insert = sqlEngine.execute(f"INSERT INTO Weights (`User`, `Weight`, `Time`) VALUES ('{user}', '{weight}', '{time}')")
+
+    return redirect('/')
+
+@app.route('/byrel')
+def rel():
+    users, rows = getUsersAndWeightList()
+    
+    rows.sort(key=lambda x: x[3], reverse=True)
+
+    rows = formatRows(rows) 
+
+    return render_template('index.htm', users=users, rows=rows)
+
+@app.route('/byweight')
+def weight():
+    users, rows = getUsersAndWeightList()
+    
+    rows.sort(key=lambda x: x[1], reverse=False)
+
+    rows = formatRows(rows) 
+    
+    return render_template('index.htm', users=users, rows=rows)
+
+@app.route('/bybmi')
+def bmi():
+    users, rows = getUsersAndWeightList()
+    
+    rows.sort(key=lambda x: x[5], reverse=False)
+        
+    rows = formatRows(rows) 
+
+    return render_template('index.htm', users=users, rows=rows)
+
+@app.route('/bytotalloss')
+def total():
+    users, rows = getUsersAndWeightList()
+    
+    rows.sort(key=lambda x: x[2], reverse=True)
+
+    rows = formatRows(rows) 
+
+    return render_template('index.htm', users=users, rows=rows)
+
+@app.context_processor
+def inject_enumerate():
+    return dict(enumerate=enumerate)
